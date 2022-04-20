d67d87427fbe2ebeeeee5f5382da5782567e8235
davidum2 <davidum215@gmail.com>
2022-04-19 22:02:47 -0500

    Flor de Dragon init

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <>
+         <ul class="menu">
+            <li><a >Home</a></li>
+            <li><a >About Us</a></li>
+            <li><a >Contact</a></li>
+        </ul>
+    </>    
+  )
+}
+
+export default Navbar
