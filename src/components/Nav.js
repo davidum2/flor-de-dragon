e3941a59e6e7cdf6387b0493d821887974373c9f
davidum2 <davidum215@gmail.com>
2022-04-29 01:18:00 -0500

    navbar entendido

---
+++ src/components/Nav.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import "./Nav.css"
+import { FaBars } from "react-icons/fa";
+
+
+
+const Nav = () => {
+
+    const [fixNavBar, setFixNavBar] = useState('nav')
+
+    const activeNavBar = () => {
+        window.scrollY > 0 ? setFixNavBar('nav active') : setFixNavBar('nav')
+    }
+
+    window.addEventListener('scroll', activeNavBar)
+
+
+    return (
+       <nav className={fixNavBar}>
+           <div className='logo'>
+              Logo
+           </div>
+           <ul className='menu'>
+               <li><Link to="/">Home</Link></li>
+               <li><Link to="/about">About</Link></li>
+               <li><Link to="/contact">Contact</Link></li>
+           </ul>
+
+           <div className='menu-btn'>
+               <FaBars/>
+           </div>
+
+       </nav>
+    )
+}
+
+export default Nav
